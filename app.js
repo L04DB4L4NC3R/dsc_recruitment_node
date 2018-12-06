3da7660b9badea4b87c17c2a98b8a324eefc0914
angad <angadsharma1016@gmail.com>
2018-12-06 15:38:57 +0530

    captcha

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bp = require("body-parser");
 const cors = require("cors");
 const app = express();
+const request = require("request");
 
 app.use(cors());
 app.use(bp.json());
@@ -15,14 +16,32 @@ mongoose.connect(process.env.DBURL, {useNewUrlParser: true});
 mongoose.connection.once("open",()=>console.log("connected"))
 .on("error",()=>console.log("error connecting to db"));
 
-app.get("/",(req,res,next)=>{
-    res.sendFile(__dirname + "/frontend/captcha.html");
-});
+// app.get("/",(req,res,next)=>{
+//     return res.send("hagga")
+//     res.sendFile(__dirname + "/frontend/captcha.html");
+// });
 app.post("/",(req,res,next)=>{
-    res.send("Submitted")
+    if(!req.body['g-recaptcha-response'])
+        return res.json({message:"Please fill captcha"})
+    
+    let verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + process.env.CAPTSECRET + "&response=" + 
+    req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+
+    request(verificationUrl,(error,response,body)=> {
+        body = JSON.parse(body);
+        // Success will be true or false depending upon captcha validation.
+        if(body.success !== undefined && !body.success) {
+          return res.json({message:"Failed captcha verification"});
+        }
+        res.redirect("/main")
+      });
+
+});
+app.get("/",(req,res,next)=>{
+    res.sendFile(__dirname + "/frontend/index.html");
 });
 app.get("/main",(req,res,next)=>{
-    res.sendFile(__dirname + "/frontend/index.html");
+    res.sendFile(__dirname + "/frontend/main.html");
 });
 
 
