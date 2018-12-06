e256d3365fdd269f035bda22395d4e6d3c5379af
angad <angadsharma1016@gmail.com>
2018-12-06 11:38:43 +0530

    captcha testing

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bp = require("body-parser");
 const cors = require("cors");
-const Recaptcha = require("express-recaptcha").Recaptcha
-let recaptcha = new Recaptcha("6LcbHX8UAAAAADZ9V7zBlLcMO-UPeXQ0zl_ch0PB", process.env.CAPTSECRET);
 const app = express();
 
 app.use(cors());
@@ -17,17 +15,17 @@ mongoose.connect(process.env.DBURL, {useNewUrlParser: true});
 mongoose.connection.once("open",()=>console.log("connected"))
 .on("error",()=>console.log("error connecting to db"));
 
-app.get("/fill",(req,res,next)=>{
+app.get("/",(req,res,next)=>{
+    res.sendFile(__dirname + "/frontend/captcha.html");
+});
+app.post("/",(req,res,next)=>{
+    res.send("Submitted")
+});
+app.get("/main",(req,res,next)=>{
     res.sendFile(__dirname + "/frontend/index.html");
 });
-app.get("/",recaptcha.middleware.render,(req,res,next)=>{
-    res.sendFile(__dirname + "/frontend/captcha.html")//({captcha:res.recaptcha});
-});
-app.post("/",recaptcha.middleware.verify,(req,res,next)=>{
-    if(!req.recaptcha.error)
-        return res.redirect("/fill");
-    res.json({message:"ROBOT identified"});
-})
+
+
 app.use("/",require("./routes/user"));
 app.use("/",require("./routes/admin"));
 
